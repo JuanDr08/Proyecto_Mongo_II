82807ca9df751c92c265141987083d1982ba44ed
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-07-28 09:13:59 -0500

    feat: :sparkles: Created the class connction to the db

--- helpers/db/connection.js
+++ helpers/db/connection.js
@@ -1,1 +1,40 @@
 import { MongoClient } from "mongodb";
+
+export class Connection {
+
+    static instance
+
+    constructor(){
+
+        if(typeof Connection.instance === 'object') {
+            return Connection.instance;
+        }
+
+        this.host = process.env.HOST;
+        this.user = process.env.MONGOUSER;
+        this.pass = process.env.PASSWORD;
+        this.cluster = process.env.CLUSTER;
+        this.port = process.env.PORT;
+    
+        this.#connect();
+        this.db = this.conexion.db(process.env.DB_NAME)
+
+        Connection.instance = this;
+
+        return this;
+
+    }
+
+    async #connect () {
+        this.conexion = new MongoClient(`${this.host}${this.user}:${this.pass}@${this.cluster}:${this.port}`);
+        await this.conexion.connect();
+    }
+
+    set setCollection(name) {
+
+        this.collection = this.db.collection(name);
+        return this.collection;
+
+    }
+
+}
