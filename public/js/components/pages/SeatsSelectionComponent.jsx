dc652f37070ccf6e3a5fdce99f2499202a00cd2e
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-27 13:54:48 -0500

    feat: :construction: Setted the seat selection component

---
+++ public/js/components/pages/SeatsSelectionComponent.jsx
@@ -0,0 +1,19 @@
+import { ButtomFooter } from "../footers/ButtomFooter"
+import { SectionIndicator } from "../headers/SectionIndicator"
+
+export const SeatsSelection = () => {
+
+    return (
+        <>
+            <SectionIndicator section='Choose seat'/>
+
+            <main>
+
+            </main>
+
+            <ButtomFooter price btnText='Buy ticket'/>
+
+        </>
+    )
+
+}
