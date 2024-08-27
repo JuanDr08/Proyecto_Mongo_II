fc169a872e07c4821758c4ad81bf58ff3a8d2604
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-27 06:55:54 -0500

    feat: :sparkles: Added react router to the project

---
+++ public/js/components/pages/HomeComponent.jsx
@@ -0,0 +1,55 @@
+import { NavBar } from '../headers/NavBar.jsx'
+import { CardsCarrusel } from '../bodys/CardsCarrusel.jsx'
+import { ComingCards } from '../bodys/ComingCards.jsx'
+import { useState } from 'react'
+import {FooterNavBar} from '../footers/PrincipalFooter.jsx'
+
+
+export const HomeComponent = () => {
+
+    const [ movie, setMovie ] = useState({title: "puss in boots the last...", gender: "Adventure"})
+
+
+    return (
+        <>
+            <NavBar name="Juan David"/>
+            <main className='w-full'>
+                <section className='flex text-xl justify-between p-[20px]'>
+                    <strong>Now playing</strong>
+                    <a href="/" className='text-red-500'>See all</a>
+                </section>
+
+                <div className='my-[10px] flex max-w-full overflow-x-scroll'>
+                    <div className='flex gap-[20px]'>
+                    <CardsCarrusel/>
+                    <CardsCarrusel/>
+                    <CardsCarrusel/>
+                    <CardsCarrusel/>
+                    </div>
+                </div>  
+
+                <section className='w-full flex flex-col justify-between capitalize items-center mt-[20px]'>
+                    <strong className='text-xl'> {movie.title} </strong>
+                    <p className='text-semiWhite'> {movie.gender} </p>
+                    <p>...</p>
+                </section>
+
+                <section className='flex flex-col gap-[20px] p-[20px]'>
+                    <div className='flex text-xl justify-between'>
+                        <strong>Coming soon</strong>
+                        <a href="/" className='text-red-500'>See all</a>
+                    </div>
+                    
+                    <div className='flex justify-center items-center gap-[15px]  mb-[100px]'>
+                        <ComingCards/>
+                    </div>
+
+                </section>
+
+                <FooterNavBar />
+
+            </main>
+        </>
+    )
+
+}
