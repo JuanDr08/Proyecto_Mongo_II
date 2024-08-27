c53d7f6a2c34f1556b8fe43be1ecf41faa8cf85f
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-27 17:16:38 -0500

    feat: :construction: Start the restructuration of the project

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use('/storage', express.static(path.join(__dirname, process.env.EXPRESS_STAT
 
 app.use((req, res, next) => { // Agregamos al objeto request la propiedad __dirname el cual contiene toda la ruta hasta nuestro directorio acutal
     req.__dirname = __dirname;
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 }, router);
 
--- public/js/components/bodys/CardsCarrusel.jsx
+++ public/js/components/bodys/CardsCarrusel.jsx
@@ -1,17 +1,12 @@
 import { Link } from "react-router-dom"
 
-export const CardsCarrusel = () => {
-
-    
-    
-
-
+export const CardsCarrusel = ({id, title, poster}) => {
 
     return (
         <>
             <div className="w-[200px] h-[350px]">
-                <Link to={'movie/1'}>
-                <img className="w-[100%] h-[100%] rounded-2xl object-cover" src="https://w0.peakpx.com/wallpaper/292/886/HD-wallpaper-puss-in-boots-dreamworks.jpg" alt="Puss in bots" />
+                <Link to={`movie/${id}`}>
+                <img className="w-[100%] h-[100%] rounded-2xl object-cover" src={`https://${poster}`} alt={title} />
                 </Link>
             </div>
         </>
