b6e05ec2d806883d475c8823b7b45df45db84448
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-29 13:56:30 -0500

    feat: :construction: finishing the seats ses section

--- src/js/components/bodys/DaysCard.jsx
+++ src/js/components/bodys/DaysCard.jsx
@@ -5,7 +5,7 @@ export const DaysCard = ({id, dia, dayName, setSelected}) => {
     const dayRef = useRef(null)
 
     return (
-        <button ref={dayRef} onClick={() => setSelected(dayRef.current, id)} className="flex w-[60px] h-[80px] flex-col p-[20px] bg-white text-black justify-center items-center rounded-lg">
+        <button ref={dayRef} onClick={() => setSelected(dayRef.current, id)} className="flex w-[70px] h-[90px] flex-col p-[20px] bg-white text-black text-xl justify-center items-center rounded-lg">
             <p><small>{dayName}</small></p>
             <p><strong>{dia}</strong></p>
         </button>
