e485950d1de91adf55aeb0fb89cb7bbe4c73ee91
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-29 22:13:44 -0500

    feat: :sparkles: Improved the carrusel movies function

--- src/js/components/pages/HomeComponent.jsx
+++ src/js/components/pages/HomeComponent.jsx
@@ -3,7 +3,7 @@ import { CardsCarrusel } from '../bodys/CardsCarrusel.jsx'
 import { ComingCards } from '../bodys/ComingCards.jsx'
 import {FooterNavBar} from '../footers/PrincipalFooter.jsx'
 // hooks
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLoaderData } from 'react-router-dom';
 
 export async function moviesLoader () {
@@ -22,7 +22,27 @@ export const HomeComponent = () => {
     const {data} = fullData;
     
     const [ movie, setMovie ] = useState({title: "puss in boots the last...", gender: "Adventure"})
+    
+    const [activeIndex, setActiveIndex] = useState(0);
+    const carouselRef = useRef(null);
 
+    
+    const moveCarousel = (offset) => {
+        const newIndex = (activeIndex + offset + data.length) % data.length;
+        setActiveIndex(newIndex);
+
+        if (carouselRef.current) {
+            carouselRef.current.style.transform = `translateX(-${newIndex * 270}px)`; 
+        }
+    };
+
+    
+    useEffect(() => {
+        setMovie({
+            title: data[activeIndex].titulo,
+            gender: data[activeIndex].genero
+        });
+    }, [activeIndex, data]);
 
     return (
         <>
@@ -34,20 +54,46 @@ export const HomeComponent = () => {
                     <a href="/" className='text-red-500'>See all</a>
                 </section>
 
-                <div className='my-[10px] flex max-w-full overflow-x-scroll'>
-                    <div className='flex gap-[20px]'>
+                <div className='my-[10px] flex max-w-full overflow-x-hidden relative'>
+                    <div className='flex gap-[20px] ml-[120px] justify-center transition-transform duration-300 ' ref={carouselRef}>
                     {
-                        data.map(({_id, title, poster}) => {
-                            return <CardsCarrusel key={_id} id={_id} title={title} poster={poster}/>
+                        data.map(({_id, title, poster}, index) => {
+                            return <CardsCarrusel 
+                                        key={_id} 
+                                        id={_id} 
+                                        title={title} 
+                                        poster={poster}
+                                        index={index}
+                                        setActiveIndex={setActiveIndex} 
+                                    />
                         })
                     }
                     </div>
+                    <button 
+                        className='absolute left-0 top-1/2 transform -translate-y-1/2 bg-semiWhite p-2 rounded-full'
+                        onClick={() => moveCarousel(-1)}
+                    >
+                        &lt; {/* Flecha izquierda */}
+                    </button>
+                    <button 
+                        className='absolute right-0 top-1/2 transform -translate-y-1/2 bg-semiWhite p-2 rounded-full'
+                        onClick={() => moveCarousel(1)}
+                    >
+                        &gt; {/* Flecha derecha */}
+                    </button>
                 </div>  
 
-                <section className='w-full flex flex-col justify-between capitalize items-center mt-[20px]'>
+                <section className='w-full flex flex-col gap-[15px] justify-between capitalize items-center mt-[20px]'>
                     <strong className='text-xl'> {movie.title} </strong>
                     <p className='text-semiWhite'> {movie.gender} </p>
-                    <p>...</p>
+                    <div className='flex'>
+                        {data.map((_, index) => (
+                            <span 
+                                key={index}
+                                className={`h-[10px] transition duration-300 rounded-full mx-1 ${index === activeIndex ? 'bg-red-500 w-[30px]' : 'bg-semiWhite w-[10px]'}`}
+                            ></span>
+                        ))}
+                    </div>
                 </section>
 
                 <section className='flex flex-col gap-[20px] p-[20px]'>
