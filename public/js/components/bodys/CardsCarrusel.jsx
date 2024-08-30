ef2bc1b590f59a71cf730706b085a43c151ea62a
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-29 22:10:35 -0500

    feat: :sparkles: created the body

---
+++ public/js/components/bodys/CardsCarrusel.jsx
@@ -0,0 +1,19 @@
+
+
+export const CardsCarrusel = () => {
+
+    
+    
+
+
+
+    return (
+        <>
+            <div className="w-[200px] h-[350px]">
+                <img className="w-[100%] h-[100%] rounded-2xl object-cover" src="https://w0.peakpx.com/wallpaper/292/886/HD-wallpaper-puss-in-boots-dreamworks.jpg" alt="Puss in bots" />
+            </div>
+        </>
+
+    )
+
+}
